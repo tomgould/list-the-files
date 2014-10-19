035b3e23dd5182f5469ee538236a2c3f03a28c02
tgould <tom.gould@turner.com>
2014-10-19 11:48:33 +0100

    Adds a gitignore for netbeans folder and some changes to the index file

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 list-the-files
 ==============
 
-I originaly thought this would be a nice way of opening media in Chromecast but the browser tab casting is not ideal for watching video but this is still useful to me so I have one place to check where I am and what I have in relation to the series and films I watch.
+I originally thought this would be a nice way of opening media in Chromecast but the browser tab casting is not ideal for watching video but this is still useful to me for looking at my media library on a single page.
 
-This provides a page where I can look at all of the episodes and films I have so I don't have to use a file explorer.
-
-I made this to work on Windows, it probably works on Unix but I havent tested it.
+I made this to work on Windows, it probably works on Unix but I haven't tested it.
--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
-
 /**
  * @file
- * Lists all my films and episodes
+ * Lists all the things
  */
 
 // Locations where my files and series are
-define('SERIES', 'C:/path/to/your/TV Shows');
-define('FILMS',  'C:/path/to/your/Films');
+define('SERIES',   'C:/path/to/TV Shows');
+define('FILMS',    'C:/path/to/Films');
+define('INCOMING', 'C:/path/to/Incoming');
 
 // Get the directories I care about
-$directories   = glob(SERIES . '/*', GLOB_ONLYDIR);
+$directories[] = INCOMING;
 $directories[] = FILMS;
+$directories = array_merge($directories, glob(SERIES . '/*', GLOB_ONLYDIR));
 
 // The file types I care about
 $types = array(
@@ -20,34 +21,49 @@ $types = array(
   '.mp4'
 );
 
-// For all the container directories (eg Series name)
-foreach ($directories as $dir) {
+// For all the directories
+foreach($directories as $dir) {
+
   // Make the printable h4 title
-  $name  = str_replace(SERIES . '/', '', $dir);
-  $name  = str_replace(FILMS, 'Films', $name);
-  $array = getDirectory($dir);
+  $name = str_replace(SERIES . '/', '', $dir);
+  $name = str_replace(FILMS, 'Films', $name);
+  $name = str_replace(INCOMING, 'Incoming', $name);
+  $array = get_directory($dir);
+
   // If there are files in this directory
   if (!empty($array)) {
     print "<h4>$name</h4>";
-    print '<ul>';
-    // write out the list
-    foreach ($array as $directory => $files) {
+    
+    // Make a sortable array
+    $container = array();
+    foreach($array as $directory => $files) {
       if (is_array($files)) {
-        foreach ($files as $file) {
-          // But only the ones worth watching ;)
+        foreach($files as $file) {
           if (!empty($types)) {
             $ext = strtolower(mb_substr($file, (4 * -1)));
             if (in_array($ext, $types)) {
               $url = $directory . '/' . $file;
-              print '<li><a href="file://' . $url . '">' . $file . '</a></li>';
+              $container[mb_strtoupper(str_replace(' ', '.', $file)) ] = array(
+                'file' => $file,
+                'url' => $url,
+              );
             }
-            // Or all of them
-          } else {
+          }
+          else {
             $url = $directory . '/' . $file;
-            print '<li><a href="file://' . $url . '">' . $file . '</a></li>';
+            $container[mb_strtoupper(str_replace(' ', '.', $file)) ] = array(
+              'file' => $file,
+              'url' => $url,
+            );
           }
         }
       }
+    }
+
+    ksort($container);
+    print '<ul>';
+    foreach($container as $item) {
+      print '<li><a href="file://' . $item['url'] . '">' . $item['file'] . '</a></li>';
     }
     print '</ul>';
   }
@@ -56,22 +72,22 @@ foreach ($directories as $dir) {
 /**
  * Get me all the things
  */
-function getDirectory($path = '.') {
-  $dirTree     = array();
+function get_directory($path = '.') {
+  $dirTree = array();
   $dirTreeTemp = array();
-  $ignore[]    = '.';
-  $ignore[]    = '..';
-  $dh          = @opendir($path);
-  
+  $ignore[] = '.';
+  $ignore[] = '..';
+  $dh = @opendir($path);
   while (false !== ($file = readdir($dh))) {
     if (!in_array($file, $ignore)) {
-      if (!is_dir("$path/$file")) {
+      if (!is_dir($path . '/' . $file)) {
         $dirTree["$path"][] = $file;
-      } else {
-        $dirTreeTemp = getDirectory("$path/$file", $ignore);
+      }
+      else {
+        $dirTreeTemp = get_directory($path . '/' . $file, $ignore);
         if (is_array($dirTreeTemp)) {
           $dirTree = array_merge($dirTree, $dirTreeTemp);
-	}
+        }
       }
     }
   }
